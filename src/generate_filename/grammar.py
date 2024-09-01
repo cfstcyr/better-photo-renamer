16dbb3bf3236a7f91e9113dd84caeeb1fa692406
Charles S <cfstcyr.git@pm.me>
2024-09-01 16:56:26 +0200

    reformat grammar

--- src/generate_filename/grammar.py
+++ src/generate_filename/grammar.py
@@ -7,16 +7,16 @@ GRAMMAR = rf"""
         | "<" TAG ":" params ">"    -> tag_params
 
     ?params: params_v | params_k
-    ?params_v: param_v ["," (params_v | params_k)] -> params_v
-    ?params_k: param_k ["," params_k] -> params_k
+    ?params_v: param_v ["," (params_v | params_k)]  -> params_v
+    ?params_k: param_k ["," params_k]               -> params_k
 
     ?param_v: STRING | NUMBER
-    ?param_k: NAME "=" param_v          -> param_k
+    ?param_k: NAME "=" param_v  -> param_k
 
     NAME: /([A-Za-z0-9]| |-|_)+/
     TAG: {" | ".join(map(lambda tag: f"\"{tag}\"", TAGS.keys()))}
 
-    %import common.ESCAPED_STRING       -> STRING
+    %import common.ESCAPED_STRING   -> STRING
     %import common.NUMBER
     %import common.WS
 

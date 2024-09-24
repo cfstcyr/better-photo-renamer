e5759f340f14606c28bd4992238bac16335d73c8
Charles S <cfstcyr.git@pm.me>
2024-09-24 09:30:26 +0200

    cleanup

--- src/metadata_extractor/metadata_extractor_image.py
+++ src/metadata_extractor/metadata_extractor_image.py
@@ -1,42 +1,18 @@
 import logging
 from datetime import datetime
 from pathlib import Path
-from pprint import pprint
 
 import piexif
 from PIL import Image
 
 from src.utils.datetime import strptime_multi
+from src.utils.exif import exif_to_tag
 from src.utils.gps import convert_gps_to_decimal
 from src.utils.hash import hash_dict
 
 from .metadata_extractor import Metadata, MetadataExtractor
 
-codec = "ISO-8859-1"  # or latin-1
-
-
 logger = logging.getLogger(__name__)
-
-
-def exif_to_tag(exif_dict):
-    exif_tag_dict = {}
-
-    for ifd in exif_dict:
-        if exif_dict[ifd] is None:
-            continue
-
-        exif_tag_dict[ifd] = {}
-
-        for tag in exif_dict[ifd]:
-            try:
-                element = exif_dict[ifd][tag].decode(codec)
-
-            except AttributeError:
-                element = exif_dict[ifd][tag]
-
-            exif_tag_dict[ifd][piexif.TAGS[ifd][tag]["name"]] = element
-
-    return exif_tag_dict
 
 
 class MetadataExtractorImage(MetadataExtractor):
---
+++ src/utils/exif.py
@@ -0,0 +1,24 @@
+import piexif
+
+codec = "ISO-8859-1"  # or latin-1
+
+
+def exif_to_tag(exif_dict):
+    exif_tag_dict = {}
+
+    for ifd in exif_dict:
+        if exif_dict[ifd] is None:
+            continue
+
+        exif_tag_dict[ifd] = {}
+
+        for tag in exif_dict[ifd]:
+            try:
+                element = exif_dict[ifd][tag].decode(codec)
+
+            except AttributeError:
+                element = exif_dict[ifd][tag]
+
+            exif_tag_dict[ifd][piexif.TAGS[ifd][tag]["name"]] = element
+
+    return exif_tag_dict

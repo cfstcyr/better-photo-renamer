65e24efb49f57b7b89f83b7beba0ced216e81e6a
Charles S <cfstcyr.git@pm.me>
2024-10-05 12:36:07 +0200

    fix auto pad error

--- library/utils/series.py
+++ library/utils/series.py
@@ -1,5 +1,6 @@
 from typing import Literal
 
+import numpy as np
 import pandas as pd
 
 Pad = int | Literal["auto"]
@@ -8,6 +9,9 @@ Pad = int | Literal["auto"]
 def pad_series(series: pd.Series, pad: Pad, fill: str = "0") -> pd.Series:
     if pad == "auto":
         pad = series.astype("str").str.len().max()
+
+        if np.isnan(pad):
+            return series
 
     series = series.astype("str").str.pad(pad, "left", fill)
 
